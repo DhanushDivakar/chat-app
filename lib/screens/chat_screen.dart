8195d0edf368067cf183ba64c9de3434a33c36a7
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-18 16:25:51 +0530

    connectedd with firease to send chats and ordered

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,9 @@
+import 'package:chat_app/widgets/chat/new_message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -19,7 +22,10 @@ class ChatScreen extends StatelessWidget {
                 child: Container(
                   child: Row(
                     children: [
-                      Icon(Icons.exit_to_app, color: Theme.of(context).primaryColor,),
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Theme.of(context).primaryColor,
+                      ),
                       SizedBox(width: 8),
                       Text("Logout"),
                     ],
@@ -36,41 +42,20 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('chats/UqZ0rEVudeXXfZqMv6IM/messages')
-              .snapshots()
-          //this gives us a stream//this is a dart object which emits a new value whenever some data source changes
-          ,
-          builder: (ctx, streamSnapshot) {
-            if (streamSnapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final documents = streamSnapshot.data.documents;
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text(documents[index]['text']),
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/UqZ0rEVudeXXfZqMv6IM/messages')
-              .add({'text': 'This was added by clikking the button'});
-          //     .listen((data) {
-          //   data.documents.forEach((document) {
-          //     print(document['text']);
-          //   });
-          //   // print(data.documents[0]['text']);
-          // }); //snapshots return a stream
-        },
-        child: Icon(Icons.add),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              //now this takes only as much space it requires
+              child: Messages(),
+            ),
+            NewMessage(),
+
+            //listview inside of a cloum dosent works good so we have to wrap it under expanded widget
+          ],
+        ),
       ),
+
     );
   }
 }
---
+++ lib/widgets/chat/new_message.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _controller = new TextEditingController();
+  var _enteredMessage = '';
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    Firestore.instance.collection('chat').add({
+      'Text': _enteredMessage,
+      'createdAt' : Timestamp.now(),
+
+    });
+    _controller.clear();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Send message...',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _enteredMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).primaryColor,
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+}
